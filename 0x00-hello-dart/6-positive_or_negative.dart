f062f4189d2ca1d05a477fe50cd91a98033b0f12
Adam Taylor <tayloradam1999@gmail.com>
2022-04-26 17:52:23 -0500

    Refactor for new source code

--- 0x00-hello-dart/6-positive_or_negative.dart
+++ 0x00-hello-dart/6-positive_or_negative.dart
@@ -1,23 +1,9 @@
-import 'dart:math';
+// Logs if the passed argument is positive, negative or zero.
 
-int _generateRandomIncludingNegative(int max) {
-  var randomGenerator = Random();
 
-  var positive = randomGenerator.nextBool();
-
-  var randInt = randomGenerator.nextInt(max);
-
-  var result = positive ? randInt : 0 - randInt;
-  return result;
-}
-
-void main() {
-  var number = _generateRandomIncludingNegative(10);
-
-  // if number is positive, print it as '$number is positive'
-  // if number is negative, print it as '$number is negative'
-  // if number is 0, print it as '$number is zero'
-  if (number > 0) print('$number is positive');
-  else if (number < 0) print('$number is negative');
-  else print('$number is zero');
+void main(List<String> args) {
+  var number = int.parse(args[0]);
+  if (number > 0) print("${number} is positive");
+  else if (number < 0) print("${number} is negative");
+  else print("${number} is zero");
 }
