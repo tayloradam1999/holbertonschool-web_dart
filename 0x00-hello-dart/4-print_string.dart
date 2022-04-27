03ae4501d89e66d01135e6e6a1d8d4892f0eebbb
Adam Taylor <tayloradam1999@gmail.com>
2022-04-27 13:41:33 -0500

    Trim file to 5 lines

--- 0x00-hello-dart/4-print_string.dart
+++ 0x00-hello-dart/4-print_string.dart
@@ -1,7 +1,3 @@
-// Prints the variable string 3 times in succession followed by a new line, then
-// prints that variables first 9 characters, followed by a new line.
-
-
 void main() {
   String str = "Holberton School";
   print('$str$str$str');
