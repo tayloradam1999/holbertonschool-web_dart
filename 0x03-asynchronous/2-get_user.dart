8a81adf2f73ae1c9cf889bfcbdfce4685051082e
Adam Taylor <tayloradam1999@gmail.com>
2022-04-28 13:04:42 -0500

    Task 2 - try/catch block

---
+++ 0x03-asynchronous/2-get_user.dart
@@ -0,0 +1,13 @@
+import '2-main.dart';
+// getUser function prints user's string representation
+// utilizes try-catch block to handle errors
+
+
+Future<void> getUser() async {
+  try {
+    final String user = await fetchUser();
+    print(user);
+  } catch (e) {
+    print('error caught: $e');
+  }
+}
