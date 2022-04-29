d596466b974b97e353706e48f30ea6f42f9480b0
Adam Taylor <tayloradam1999@gmail.com>
2022-04-28 20:24:12 -0500

    Reformat code

--- 0x03-asynchronous/Quotes/quotes.dart
+++ 0x03-asynchronous/Quotes/quotes.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'dart:async';
 // generateQuote() accepts one argument 'id' and returns the quote associated with that id.
 // url: https://breakingbadapi.com/api/quotes
 // utilizes try-catch to handle errors.
@@ -9,15 +10,11 @@ Future<String> generateQuote(id) async {
   try {
     // fetch data from url
     // Uri is from dart:core
-    final response = await http.get(
-      Uri.parse('https://breakingbadapi.com/api/quotes/$id'),
-    );
+    var response = await http.get(Uri.parse('https://breakingbadapi.com/api/quotes/$id'), headers: {"Accept": "application/json"});
     // decode json data
-    final jsonData = json.decode(response.body);
-    // return first quote fetched from API in this format:
-    String charName = jsonData[0]['author'];
-    String quote = jsonData[0]['quote'];
-    return '$charName: $quote';
+    var jsonData = json.decode(response.body);
+    // return the quote
+    return jsonData[0]['author'] + ': ' + jsonData[0]['quote'];
   } catch (e) {
     // print error
     return('There is no quotes');
