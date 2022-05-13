09986c5be95c4f5afa2e784f52df9cfe4d1b182d
Adam Taylor <tayloradam1999@gmail.com>
2022-05-12 20:03:05 -0500

    Change code to meet checker requirements

--- 0x01-dart-function/5-calculate-area.dart
+++ 0x01-dart-function/5-calculate-area.dart
@@ -2,18 +2,7 @@
 
 
 double calculateArea(double height, double base) {
-  double area = (height * base) / 2;
-  // check if area has decimals
-  if (area.toString().contains('.')) {
-    // if so, lets round it to 2 decimals
-    String area2 = area.toStringAsFixed(2);
-    // convert to double
-    area = double.parse(area2);
-    // return the double
-    return area;
-  }
-  // if not, return the double
-  return area;
+  return (height * base);
 }
 
 // area could have started as String looking back at this code.
--- 0x01-dart-function/5-main.dart
+++ 0x01-dart-function/5-main.dart
@@ -2,6 +2,6 @@ import '5-calculate-area.dart';
 
 main() {
   print(calculateArea(30,20));
-  print(calculateArea(10,7));
+  print(calculateArea(2.4,3.5));
   print(calculateArea(8.5,3.7));
 }
