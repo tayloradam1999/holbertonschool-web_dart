77e4728559d475ab3f8235a577a30cffe9f57039
Adam Taylor <tayloradam1999@gmail.com>
2022-05-12 20:14:10 -0500

    Checker refactoring

--- 0x01-dart-function/6-convert-to-fahrenheit.dart
+++ 0x01-dart-function/6-convert-to-fahrenheit.dart
@@ -6,6 +6,11 @@ List<double> convertToF(List<double> temperaturesInC) {
   List<double> temperaturesInF = [];
   for (var i = 0; i < temperaturesInC.length; i++) {
     String tempInF = ((temperaturesInC[i] * 9 / 5) + 32).toStringAsFixed(2);
+    // check if the number holds two decimal places ex: '25.00'
+    // if it doesn't, fill it with zeros.
+    if (tempInF.split(".")[1].length < 3) {
+      tempInF = tempInF + "0";
+    }
     temperaturesInF.add(double.parse(tempInF));
   }
   return temperaturesInF;
